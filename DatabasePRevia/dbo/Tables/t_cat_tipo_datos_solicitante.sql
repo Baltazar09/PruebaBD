1e62d786ef63b2732e1697fcc8b9d5feae5fe9f5
jose zamora <jzamora@cgclatam.com>
2021-02-17 11:30:47 -0600

    V0.1 - TABLAS PARA GESTIONES 5.10,5.11,5.12,5.13

---
+++ DatabasePRevia/dbo/Tables/t_cat_tipo_datos_solicitante.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[t_cat_tipo_datos_solicitante] (
+    [PK_TIPO_DATOS_SOLICITANTE] INT            IDENTITY (1, 1) NOT NULL,
+    [DESCRIPCION]               NVARCHAR (250) NULL,
+    [STATUS] BIT NULL, 
+    CONSTRAINT [PK_t_cat_datos_solicitante] PRIMARY KEY CLUSTERED ([PK_TIPO_DATOS_SOLICITANTE] ASC)
+);
+
---
+++ DatabasePRevia/dbo/Tables/t_ges_formalizacion_servicio.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[t_ges_formalizacion_servicio] (
+    [PK_FORMALIZACION_SERVICIO] INT            NOT NULL,
+    [OBSERVACIONES]             NVARCHAR (500) NULL,
+    [URL_CERTIFICACION_PODER]   NVARCHAR (250) NULL,
+    [URL_CEDULA_FRENTE]         NVARCHAR (250) NULL,
+    [URL_CEDULA_ATRAS]          NVARCHAR (250) NULL,
+    [AUTORIZACION_ABOGADO]      BIT            NULL,
+    [STATUS]                    BIT            NULL,
+    CONSTRAINT [PK_t_ges_formalizacion_servicio] PRIMARY KEY CLUSTERED ([PK_FORMALIZACION_SERVICIO] ASC),
+    CONSTRAINT [FK_t_ges_formalizacion_servicio_t_ges_gestion] FOREIGN KEY ([PK_FORMALIZACION_SERVICIO]) REFERENCES [dbo].[t_ges_gestion] ([PK_GESTION])
+);
+
